2746b1071740503d21ad2aae073a38e85538311c
Shuhaib77 <shuhaibbasheer20022@gmail.com>
2025-12-31 12:04:12 +0530

    deployment error fixed

--- authe/webpack.config.js
+++ authe/webpack.config.js
@@ -1,42 +1,112 @@
+// const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const { ModuleFederationPlugin } = require("webpack").container;
+
+// module.exports = {
+//   mode: "development",
+//   entry: "./src/index.tsx",
+//   devServer: {
+//     port: 3003,
+//     historyApiFallback: true,
+//   },
+//   output: {
+//     publicPath: "auto",
+//   },
+//   resolve: {
+//     extensions: [".ts", ".tsx", ".js", ".jsx"],
+//   },
+    
+//   module: {
+//     rules: [
+//       // Add this rule for TypeScript/JSX
+//       {
+//         test: /\.(ts|tsx|js|jsx)$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader',
+//         },
+//       },
+//       // CSS rule
+//       {
+//         test: /\.css$/,
+//         use: [
+//           'style-loader',
+//           'css-loader',
+//           'postcss-loader',
+//         ],
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new ModuleFederationPlugin({
+//       name: "authe",
+//       filename: "remoteEntry.js",
+//       exposes: {
+//         "./App": "./src/App",
+//       },
+//       shared: {
+//         react: { 
+//           singleton: true,
+//           eager: true,
+//           requiredVersion: "^18.0.0"
+//         },
+//         "react-dom": { 
+//           singleton: true,
+//           eager: true,
+//           requiredVersion: "^18.0.0"
+//         },
+//         "react-router-dom": { 
+//           singleton: true,
+//           eager: true,
+//           requiredVersion: "^6.0.0"
+//         },
+//       }
+//     }),
+//     new HtmlWebpackPlugin({
+//       template: "./public/index.html",
+//     }),
+//   ],
+// };
+
+
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { ModuleFederationPlugin } = require("webpack").container;
 
 module.exports = {
-  mode: "development",
+  mode: "production",        // <-- IMPORTANT
   entry: "./src/index.tsx",
+
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),  // <-- IMPORTANT
+    publicPath: "auto",
+    clean: true
+  },
+
   devServer: {
     port: 3003,
     historyApiFallback: true,
   },
-  output: {
-    publicPath: "auto",
-  },
+
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".jsx"],
   },
-    
+
   module: {
     rules: [
-      // Add this rule for TypeScript/JSX
       {
         test: /\.(ts|tsx|js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+        use: "babel-loader",
       },
-      // CSS rule
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'postcss-loader',
-        ],
+        use: ["style-loader", "css-loader", "postcss-loader"],
       },
     ],
   },
+
   plugins: [
     new ModuleFederationPlugin({
       name: "authe",
@@ -45,23 +115,12 @@ module.exports = {
         "./App": "./src/App",
       },
       shared: {
-        react: { 
-          singleton: true,
-          eager: true,
-          requiredVersion: "^18.0.0"
-        },
-        "react-dom": { 
-          singleton: true,
-          eager: true,
-          requiredVersion: "^18.0.0"
-        },
-        "react-router-dom": { 
-          singleton: true,
-          eager: true,
-          requiredVersion: "^6.0.0"
-        },
-      }
+        react: { singleton: true, eager: true },
+        "react-dom": { singleton: true, eager: true },
+        "react-router-dom": { singleton: true, eager: true },
+      },
     }),
+
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
--- packages/shared-config/webpack.common.js
+++ packages/shared-config/webpack.common.js
@@ -21,99 +21,39 @@
 
 
 
-// const path = require("path");
-// const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const { ModuleFederationPlugin } = require("webpack").container;
-
-// module.exports = {
-//   mode: "development",
-//   entry: "./src/index.tsx",
-//   devServer: {
-//     port: 3004, // Different port
-//     historyApiFallback: true,
-//   },
-//   output: {
-//     publicPath: "auto",
-//   },
-//   resolve: {
-//     extensions: ['.ts', '.tsx', '.js', '.jsx'],
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(ts|tsx|js|jsx)$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: 'babel-loader',
-//         },
-//       },
-//       {
-//         test: /\.css$/,
-//         // No exclude - processes ALL CSS
-//         use: ['style-loader', 'css-loader', 'postcss-loader'],
-//       },
-//     ],
-//   },
-//   plugins: [
-//     new ModuleFederationPlugin({
-//       name: "patient",
-//       filename: "remoteEntry.js",
-//       exposes: {
-//         "./App": "./src/App",
-//       },
-//       shared: {
-//         react: { singleton: true, eager: true },
-//         "react-dom": { singleton: true, eager: true },
-//         "react-router-dom": { singleton: true, eager: true },
-//       },
-//     }),
-//     new HtmlWebpackPlugin({
-//       template: "./public/index.html",
-//     }),
-//   ],
-// };
-
-
-
-
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { ModuleFederationPlugin } = require("webpack").container;
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   entry: "./src/index.tsx",
-
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "auto",
-    clean: true
-  },
-
   devServer: {
-    port: 3004,
+    port: 3004, // Different port
     historyApiFallback: true,
   },
-
+  output: {
+    publicPath: "auto",
+  },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
   },
-
   module: {
     rules: [
       {
         test: /\.(ts|tsx|js|jsx)$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: {
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.css$/,
+        // No exclude - processes ALL CSS
         use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
     ],
   },
-
   plugins: [
     new ModuleFederationPlugin({
       name: "patient",
@@ -127,9 +67,10 @@ module.exports = {
         "react-router-dom": { singleton: true, eager: true },
       },
     }),
-
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
   ],
 };
+
+
