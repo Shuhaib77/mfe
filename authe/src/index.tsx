a4927129623ca9320feef4f22f20dbb83dfe76ad
Shuhaib77 <shuhaibbasheer20022@gmail.com>
2025-12-31 11:16:19 +0530

    ts error fixed

--- authe/src/index.tsx
+++ authe/src/index.tsx
@@ -5,7 +5,7 @@
 
 // const root = createRoot(document.getElementById("root")!);
 // root.render(<App />);
-import React from "react";
+
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
