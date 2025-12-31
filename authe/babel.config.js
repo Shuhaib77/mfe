451c729630b1c1f4bc084db801598c73c5649195
Shuhaib77 <shuhaibbasheer20022@gmail.com>
2025-12-31 11:19:00 +0530

    ts error fixed

--- authe/babel.config.js
+++ authe/babel.config.js
@@ -1,9 +1,9 @@
-// module.exports = {
-//   presets: [
-//     '@babel/preset-env',
-//     ['@babel/preset-react', { runtime: 'automatic' }],
-//     '@babel/preset-typescript',
-//   ],
-// };
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    ['@babel/preset-react', { runtime: 'automatic' }],
+    '@babel/preset-typescript',
+  ],
+};
 
 module.exports = require('@myorg/shared-config/babel.config');
