98e544643446ac198401c7c94f5419795b263316
Shuhaib77 <shuhaibbasheer20022@gmail.com>
2025-12-31 11:59:22 +0530

    deployment error fixed

--- packages/shared-config/webpack.common.js
+++ packages/shared-config/webpack.common.js
@@ -19,39 +19,101 @@
 //   },
 // };
 
+
+
+// const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const { ModuleFederationPlugin } = require("webpack").container;
+
+// module.exports = {
+//   mode: "development",
+//   entry: "./src/index.tsx",
+//   devServer: {
+//     port: 3004, // Different port
+//     historyApiFallback: true,
+//   },
+//   output: {
+//     publicPath: "auto",
+//   },
+//   resolve: {
+//     extensions: ['.ts', '.tsx', '.js', '.jsx'],
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(ts|tsx|js|jsx)$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader',
+//         },
+//       },
+//       {
+//         test: /\.css$/,
+//         // No exclude - processes ALL CSS
+//         use: ['style-loader', 'css-loader', 'postcss-loader'],
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new ModuleFederationPlugin({
+//       name: "patient",
+//       filename: "remoteEntry.js",
+//       exposes: {
+//         "./App": "./src/App",
+//       },
+//       shared: {
+//         react: { singleton: true, eager: true },
+//         "react-dom": { singleton: true, eager: true },
+//         "react-router-dom": { singleton: true, eager: true },
+//       },
+//     }),
+//     new HtmlWebpackPlugin({
+//       template: "./public/index.html",
+//     }),
+//   ],
+// };
+
+
+
+
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { ModuleFederationPlugin } = require("webpack").container;
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/index.tsx",
+
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "auto",
+    clean: true
+  },
+
   devServer: {
-    port: 3004, // Different port
+    port: 3004,
     historyApiFallback: true,
   },
-  output: {
-    publicPath: "auto",
-  },
+
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
   },
+
   module: {
     rules: [
       {
         test: /\.(ts|tsx|js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+        use: "babel-loader",
       },
       {
         test: /\.css$/,
-        // No exclude - processes ALL CSS
         use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
     ],
   },
+
   plugins: [
     new ModuleFederationPlugin({
       name: "patient",
@@ -65,6 +127,7 @@ module.exports = {
         "react-router-dom": { singleton: true, eager: true },
       },
     }),
+
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
